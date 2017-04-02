f34f522d9b1f4e19fcbf28d8144e28520a74d621
tomctomc <tomctomc@users.noreply.github.com>
2017-04-02 13:18:07 -0400

    cleanup comments and add unistd.h for unlink()

--- a09.c
+++ a09.c
@@ -230,6 +230,7 @@
 #include <stdarg.h>
 #include <stdlib.h>
 #include <time.h>
+#include <unistd.h>
 
 /*****************************************************************************/
 /* Definitions                                                               */
